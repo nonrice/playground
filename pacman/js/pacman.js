32d89c9336460d6e26d777f9619ede2d0618ad7d
Eric Xue <widefin@gmail.com>
2016-07-05 16:26:07 -0400

    updated pacman

--- pacman/js/pacman.js
+++ pacman/js/pacman.js
@@ -1,20 +1,46 @@
 var tileTypes = {
-    "_": "tile0.gif",
-    ".": "pnt.gif",
-    "*": "pill.gif",
-    "1": "tile1.gif",
-    "2": "tile2.gif"
+    " ": "pnt.gif",
+    "p": "pill.gif",
+    "x": "tile0.gif",
+    "l": "tile1.gif",
+    "m": "tile2.gif",
+    "h": "tile3.gif",
+    "o": "tile4.gif",
+    "b": "tile5.gif",
+    "d": "tile6.gif",
+    "f": "tile7.gif",
+    "n": "tile8.gif",
+    "a": "tile9.gif",
+    "c": "tile10.gif",
+    "i": "tile11.gif",
+    "v": "tile12.gif",
+    "e": "tile13.gif",
+    "g": "tile14.gif",
+    //"g": "tile15.gif",
+    "k": "tile16.gif",
+    "q": "tile17.gif",
+    "t": "tile18.gif",
+    "r": "tile19.gif"
 };
+
 
 function drawMaze(element, tiles) {
     var html="";
     for(var row=0; row<tiles.length; row++){
+        html += '<div class="row">';
         for(var column=0; column<tiles[row].length; column++){
             var tileType = tiles[row][column];
             var img=tileTypes[tileType];
             html+="<img src=\"images/" + img + "\" alt=\"\">";
         }
-        html+="<br>"
+        html+="</div>"
     }
     element.innerHTML = html;
+}
+
+
+
+function drawPacman(pacman, pacmanInfo) {
+    pacman.style.left = (pacmanInfo.column * 27 + maze.offsetLeft) + "px";
+    pacman.style.top = (pacmanInfo.row * 27 + maze.offsetTop) + "px";
 }
